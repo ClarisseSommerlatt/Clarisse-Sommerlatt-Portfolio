9e571df2cba1ac2f79200f195ef3f5d04532b285
ClarisseSommerlatt <114338476+ClarisseSommerlatt@users.noreply.github.com>
2023-01-08 20:04:07 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,36 +1,43 @@
-//console.log("Tout fonctionne")
+// Quand on clique sur un Bouton :
+// Des rectangles sont animés
+// L'animation fonctionne dans les deux sens
 
-//const h1 = document.querySelector("h1");
-//let color = "red"
+// I Créer les blocs en HTML
+// II Créer les styles CSS (couleur sur nos blocs, transition CSS)
+// III JS : 
+// 1. Selectionner le bouton + cartes/rectangles
+// 2. Implémenter l'ecoute d'un évènement clique
+// 3. Action qui s'applique / Fonction
 
-//color = "blue"
-
-//h1.style.color = "green"
-// document.querySelector("h1").style.color = "red"
-//class
-// body.classList.add("dark")
-
-//changer couleur tire à l'aide d'un bouton
-//button.addEventListener('click', function () {
-
-    //if (h1.style.color === "pink") {
-       // h1.style.color = "purple"
-    //} else {
-        // h1.style.color = "pink"}
-//})
-
-// Selecteurs
-const body = document.querySelector("body");
-const h1 = document.querySelector("h1");
 const button = document.querySelector("button");
+// const container = document.querySelector(".container");
+const cards = document.querySelectorAll('.cards');
 
-// Listener
-button.addEventListener("click", function() {
-  if (body.classList.contains("light")) {
-    body.classList.remove("light");
-    body.classList.add("dark");
-  } else {
-    body.classList.add("light");
-    body.classList.remove("dark");
-  }
+  console.log(button);
+  console.log(document.querySelector('.cards'));
+  console.log(document.querySelectorAll('.cards'));
+
+button.addEventListener("click", function () {
+   
+  
+  // if (container.classList.contains("active")) {
+  //  container.classList.remove("active"); 
+  // } else {
+  //   container.classList.add("active");
+  // }
+ 
+  
+    // cards[0].classList.toggle("active");
+    // cards[1].classList.toggle("active");
+    // cards[2].classList.toggle("active");
+    // cards[3].classList.toggle("active");
+    // cards[4].classList.toggle("active");
+    // cards[5].classList.toggle("active");
+    // cards[6].classList.toggle("active");
+    // cards[7].classList.toggle("active");
+  
+    for (i = 0; i < 5; i++) {
+      cards[i].classList.toggle("active");
+      cards[i].style.transitionDelay = (0.25 * i) + "s";
+    }
 });
